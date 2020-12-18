4ef0e79842275282deeb106af520fbc22a0c521b
Andrew <x.and.i.rew.e@gmail.com>
2020-12-18 21:02:50 +1100

    first commit

---
+++ bot.py
@@ -0,0 +1,64 @@
+import json
+import discord
+import requests
+import random
+
+def random_quote():
+    # not in quote mode
+    if not settings['quote_mode']:
+        return random.choice(settings['positive_words'])
+    # quote mode
+    response = requests.get('https://zenquotes.io/api/random/')
+    if response.status_code >= 400:
+        return "Be happy like me, Happy Bot! XD"
+    content = json.loads(response.text)[0]
+    return f'Be happy like me, Happy Bot!\n{content["q"]}\n\t- {content["a"]}'
+
+settings = {
+    'trigger': '(:',
+    'quote_mode': True,
+    'negative_words': ['depressed', 'depression', 'sad', 'bad', 'kms', 'no', 'angry', 'die'],
+    'positive_words': ['Be happy like me, Happy Bot! XD', 'You have summoned me, Happy Bot! XD'],
+}
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is ready to spread some positivity!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or message.author.bot:
+        return
+    # turn off quote mode
+    if message.content == settings['trigger']:
+        settings['quote_mode'] = False
+        await message.channel.send(random_quote())
+        return
+    # turn on quote mode
+    if message.content.lower() == f"{settings['trigger']} quote":
+        settings['quote_mode'] = True
+        await message.channel.send(random_quote())
+        return
+    
+    # check messages for negative words
+    parsed_messages = message.content.split(' ')
+    sent = False
+    for index, word in enumerate(parsed_messages):
+        if sent:
+            break
+        if word in settings['negative_words']:
+            if index > 0 and parsed_messages[index - 1].lower() == 'not':
+                continue
+            await message.channel.send(random_quote())
+            sent = True
+
+
+# run bot
+with open('token', 'r') as f:
+    lines = f.readlines()
+client.run(lines[0])
+
+# TODO: implement persistence
